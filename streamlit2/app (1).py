7450b5aa13de97dd6c2987fb81ab3b06d0307b53
stiltnerag <stiltnerag@vcu.edu>
2026-06-24 19:55:59 -0400

    Update app (1).py

--- streamlit2/app (1).py
+++ streamlit2/app (1).py
@@ -6,7 +6,6 @@ import streamlit as st
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "churn_model.pkl")
 
-with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
 
 
